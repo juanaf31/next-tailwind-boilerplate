763ec1c2d963a3f3623d00083004c4c53591db1d
Juan <johndoe@email.com>
2023-08-16 17:52:04 +0700

    revert: :rewind: tes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ function DahsboardPage() {
   return (
     <div className="min-h-screen p-4">
       <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="rounded-lg bg-white px-5 py-4">Label 11</div>
+        <div className="rounded-lg bg-white px-5 py-4">Label 1</div>
         <div className="rounded-lg bg-white px-5 py-4">Label 2</div>
       </div>
     </div>
